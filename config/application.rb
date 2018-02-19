4a3878a969c39a5d22caeca4989875e13b3f473c
Oskar Brännström <oskar@quad.se>
2018-02-19 15:11:55 +0100

    Finished up installation with Rspec

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,14 @@ module RailsDemo
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.generators do |generate|
+      generate.helper false
+      generate.assets false
+      generate.view_specs false
+      generate.helper_specs false
+      generate.routing_specs false
+      generate.controller_specs false
+      generate.system_tests false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
